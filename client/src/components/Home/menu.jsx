afe10f840ae1c49bca52eb13e1e8ce5971116d57
Md Alamin <mdalamin.eu@gmail.com>
2020-06-22 16:46:47 +0300

    Logout

--- client/src/components/Home/menu.jsx
+++ client/src/components/Home/menu.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
+import React,{Fragment, Component} from 'react'
 import {Link} from 'react-router-dom'
-import  { logout } from '../utlis/auth'
-const Menu = () =>{
-    const logouta = () =>  {
+import  { isAuth ,logout } from '../utlis/auth'
+class Menu extends Component {
+
+    
+     logouta = () =>  {
         console.log('hi', logout)
 logout()
     }
+  
+    
+      NotLoggedin = (
+    <Fragment>
+                     <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+    </Fragment>
+    )
+
+     loggedinn = (
+        <Fragment>
+                 <li>
+                    <Link onClick={this.logouta}>Logout</Link>
+                </li>
+                
+    </Fragment>
+    )
+
+ render () {
     return(
         <div>
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                <li>
-                    <Link onClick={logouta}>Logout</Link>
-                </li>
+               { isAuth() && isAuth().loggedin ? this.loggedinn : this.NotLoggedin}
             </ul>
         </div>
     )
+ }
+    
 }
 
 export default Menu
